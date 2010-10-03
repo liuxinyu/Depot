8228d72983b211b3816d0a26e8bcf05ebcf2c5ab
unknown <LXY@.(none)>
2010-10-03 23:02:41 +0800

    after_chap10

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,12 +2,16 @@
 class StoreController < ApplicationController
   def index
     @products = Product.find_products_for_sale
+    @cart = find_cart
   end
   
   def add_to_cart
     product = Product.find(params[:id])
     @cart = find_cart
-    @cart.add_product(product)
+    @current_item = @cart.add_product(product)
+    respond_to do |format|
+      format.js
+    end
    rescue ActiveRecord::RecordNotFound
     logger.error("Attempt to access invalid produc #{params[:id]}")
     redirect_to_index("Invalid product")
@@ -18,8 +22,29 @@ class StoreController < ApplicationController
     redirect_to_index("Your cart is currently empty")
   end
 
+  def checkout
+    @cart = find_cart
+    if @cart.items.empty?
+      redirect_to_index("Your cart is empty")
+    else
+      @order = Order.new
+    end
+  end
+  
+  def save_order
+    @cart = find_cart
+    @order = Order.new(params[:order])
+    @order.add_line_items_from_cart(@cart)
+    if @order.save
+      session[:cart] = nil
+      redirect_to_index("Thank you for your order")
+    else
+      render :action => 'checkout'
+    end
+  end
+  
 private
-  def redirect_to_index(msg)
+  def redirect_to_index(msg = nil)
     flash[:notice] = msg
     redirect_to :action => 'index'
   end
